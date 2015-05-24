3ae0d0a8e3104730ff09501d0d86577d03397661
Andrew Mutz <andrew.mutz@gmail.com>
2015-05-23 20:47:50 -0700

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -31,8 +31,8 @@ defmodule EctoRiakAdapter.Mixfile do
       {:ecto,    ">= 0.2.4"},
       {:decimal, ">= 0.2.5"},
       {:poison,  github: "devinus/poison"},
-      {:riakc,   github: "basho/riak-erlang-client", tag: "2.1.0.2"},
-      {:riak_pb, github: "basho/riak_pb", compile: "./rebar get-deps compile deps_dir=../", override: true, tag: "2.1.0.2"}
+      {:riakc,   github: "basho/riak-erlang-client", tag: "2.1.0.1"},
+      {:riak_pb, github: "basho/riak_pb", compile: "./rebar get-deps compile deps_dir=../", override: true, tag: "2.1.0.1"}
     ]
   end
 end
